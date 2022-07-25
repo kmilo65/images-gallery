a17b0755a18debf594445d95a82f16384192af7d
kmilo <cfhg65@gmail.com>
2022-07-24 20:30:54 -0400

    init header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
 
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>Images Gallery</h1>
+    <div>
+      <Header title="Images Gallery"/>
     </div>
   );
 }
 
 export default App;
+
