df923b03bd0e227406e58ce5587569c96c32308b
kmilo <cfhg65@gmail.com>
2022-07-27 20:22:13 -0400

    header and search components

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,18 @@
 
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
+import { Navbar,Container} from 'react-bootstrap';
+
+const navbarStyle={
+  backgroundColor:'ligthblue'
+};
 
 const Header=(props) =>{
     return (
-        <Navbar bg="light" variant="light">
-          <Navbar.Brand href="/">{props.title}</Navbar.Brand>
+        <Navbar style={navbarStyle} variant="light">
+          <Container>
+            <Navbar.Brand href="/">{props.title}</Navbar.Brand>
+          </Container>
+          
         </Navbar>
     )
 };
