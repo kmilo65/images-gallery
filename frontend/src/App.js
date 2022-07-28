b35c36a2b527b9f90eb8ffcc5179366ad56e58b0
kmilo <cfhg65@gmail.com>
2022-07-27 20:03:19 -0400

    search component states

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,29 @@
+import {useState} from 'react';
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
+import Search from './components/Search';
+
 
 const App = () => {
+  
+  const [word,setword]=useState('');
+
+  const handleSearchSubmit=(e)=>{
+    e.preventDefault();
+    console.log(word);
+  }
+
+  console.log(word);
+
   return (
     <div>
       <Header title="Images Gallery"/>
+      <Search word={word} setword={setword} handleSubmit={handleSearchSubmit}/>
     </div>
   );
 }
 
 export default App;
+
 
